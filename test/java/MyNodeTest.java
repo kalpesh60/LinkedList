7d761857189834bf57453c1e661fab00705e39ff
kalpesh60 <kalpeshdhalpe60@gmail.com>
2021-05-14 17:21:29 +0530

    Created Simple LinkedList

--- test/java/MyNodeTest.java
+++ test/java/MyNodeTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 public class MyNodeTest {
 
     @Test
-    public void given3NumbersWhenLinkedShouldPassedLinkedListTest() {
+    public void givenThreeNumbersWhenLinkedShouldPassedLinkedListTest() {
         MyNode<Integer> myFirstNode = new MyNode<>(56);
         MyNode<Integer> mySecondNode = new MyNode<>(30);
         MyNode<Integer> myThirdNode = new MyNode<>(70);
