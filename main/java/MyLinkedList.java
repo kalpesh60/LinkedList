1dbf5ca44ad5e0cbfeac0d19024e25834a3ede25
kalpesh60 <kalpeshdhalpe60@gmail.com>
2021-05-14 20:01:44 +0530

    Created LinkedList using append

--- main/java/MyLinkedList.java
+++ main/java/MyLinkedList.java
@@ -20,6 +20,18 @@ public class MyLinkedList<K> {
         }
     }
 
+    public void append(INode<K> newNode) {
+        if(this.tail == null) {
+            this.tail = newNode;
+        }
+        if(this.head == null) {
+            this.head = newNode;
+        } else {
+            this.tail.setNext(newNode);
+            this.tail = newNode;
+        }
+    }
+
     public void printMyNodes() {
         StringBuffer myNodes = new StringBuffer("My Nodes:");
         INode<K> tempNode = head;
