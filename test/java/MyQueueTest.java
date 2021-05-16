a40f0137ac4b266abef7c9fc29efcd9ae45f1772
kalpesh60 <kalpeshdhalpe60@gmail.com>
2021-05-16 23:15:23 +0530

    Added code for creating a Queue

--- test/java/MyQueueTest.java
+++ test/java/MyQueueTest.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 
 public class MyQueueTest {
     @Test
-    public void given3NumberWhenAddedToQueueShouldHaveLastAddedNode() {
+    public void given3NumberWhenAddedToQueueShouldHaveFirstAddedNode() {
         MyNode<Integer> myFirstNode = new MyNode<>(56);
         MyNode<Integer> mySecondNode = new MyNode<>(30);
         MyNode<Integer> myThirdNode = new MyNode<>(70);
