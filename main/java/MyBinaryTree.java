db9a59d4753c05b14337834f98cf74e89d4e0dd4
kalpesh60 <kalpeshdhalpe60@gmail.com>
2021-05-19 23:30:26 +0530

    Creating a Binary Search Tree

---
+++ main/java/MyBinaryTree.java
@@ -0,0 +1,29 @@
+public class MyBinaryTree<K extends Comparable<K>> {
+    private MyBinaryNode<K> root;
+
+    public void add(K key) {
+        this.root = this.addRecursively(root, key);
+    }
+
+    private MyBinaryNode<K> addRecursively(MyBinaryNode<K> current, K key) {
+        if (current == null)
+            return new MyBinaryNode<>(key);
+        int compareResult = key.compareTo(current.key);
+        if (compareResult == 0) return current;
+        if (compareResult < 0) {
+            current.left = addRecursively(current.left, key);
+        } else {
+            current.right = addRecursively(current.right, key);
+        }
+        return current;
+    }
+
+    public int getSize() {
+        return this.getSizeRecursive(root);
+    }
+
+    private int getSizeRecursive(MyBinaryNode<K> root) {
+        return root == null ? 0 : 1 + this.getSizeRecursive(root.left)
+                +this.getSizeRecursive(root.right);
+    }
+}
